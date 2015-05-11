23513f5e6dc7c4e12e470a2ff74f48ff51ba2acd
luis.villanueva <luis.villanueva@interalia.net>
2015-05-11 12:13:01 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'tastypie',
+        'django-tastypie',
         'django-recaptcha'
     ],
     license="MIT",
